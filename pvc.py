ce1120a071d0c65e742588eef8d5ee88c6525236
Chris T. Mackey <chris.t.mackey@gmail.com>
2016-11-18 08:40:08 -0500

    Update pvc.py
    adding license

--- pvc.py
+++ pvc.py
@@ -1,3 +1,26 @@
+"""pvc:   Tools for costing and rapid prototyping of Poly vinylchloride (PVC).
+    Copyright (C) 2016  Chris T. Mackey
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    To contribute to this code, for questions, custom installations,
+    or polymer consulting contact me via email or github.
+    
+    chris.t.mackey AT gmail DOT com
+    https://github.com/chrisTMackey
+    """
+
 def main():
     sample_formula = [(100, 1.4, 0.475), (28, 0.973, 0.92), (7.5, 1.008, 1.56),
                       (0, 2.71, 0.0625)]
